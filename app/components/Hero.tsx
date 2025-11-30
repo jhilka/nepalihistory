c54a33681be1b425bc53c6de3f35b0924d4b3363
bkrmadtya <shinethelights101@gmail.com>
2025-11-30 21:12:41 +0200

    feat: refine Hero component styling for improved readability and layout

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,10 +2,10 @@ import { HERO } from "@/app/constants";
 
 const Hero = () => (
   <header className="min-h-54 flex flex-col items-center justify-center gap-4 mb-10">
-    <span className="text-black/80 text-xs bg-stone-200  px-3 py-1 rounded-xl">
+    <span className="text-black/80 text-[10px] sm:text-xs bg-stone-200 px-3 py-1 rounded-xl">
       {HERO.preface}
     </span>
-    <h1 className="text-3xl md:text-4xl font-bold font-oswald capitalize tracking-tight text-balance">
+    <h1 className="text-3xl md:text-4xl font-bold font-oswald capitalize tracking-tight text-balance my-1">
       <span className="text-national-maroon">{HERO.title.split(" ")[0]} </span>
       {HERO.title.split(" ").slice(1).join(" ")}
     </h1>
