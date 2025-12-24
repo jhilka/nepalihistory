06e1c2b7921bb9f0be65bcbfc19840ba93819cc8
jhilka <shinethelights101@gmail.com>
2025-12-24 15:05:44 +0200

    update: added cache headers and path for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,33 @@
+const getPath = () => {
+  if (process.env.IS_GITHUB_PAGES === "true") {
+    return "/nepalihistory";
+  }
+  return "";
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/nepalihistory" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/nepalihistory/" : "",
+  basePath: getPath(),
+  assetPrefix: getPath(),
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: "/timelines/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 };
 
